731c0ea46ec01df7edc55b59579f9f807a04f124
vanedaq <90211990+vanedaq@users.noreply.github.com>
2025-09-14 20:17:52 -0500

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,39 @@
-const CACHE = "finanzas-v1";
+const CACHE_NAME = 'organizador-v1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.app.css",
-  "./app.app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  '/',
+  '/index.html',
+  '/style.app.css',
+  '/app.app.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-      return (
-        r ||
-        fetch(e.request).then((res) => {
-          const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(e.request, copy));
-          return res;
-        })
-      );
-    })
+self.addEventListener('activate', event => {
+  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // network-first fallback to cache
+  event.respondWith(
+    fetch(event.request).then(resp=>{
+      if(event.request.method === 'GET' && resp && resp.status === 200){
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then(c=>c.put(event.request, copy));
+      }
+      return resp;
+    }).catch(()=>caches.match(event.request).then(r=>r||new Response('',{status:404})))
   );
 });
